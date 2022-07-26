98a76c45a11324d83118aa821d631baa5fcc34b1
ezhuezhu07 <ezhun@egnaroinc.com>
2022-07-26 21:24:32 +0530

    initial-commit

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'TodoApp';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
